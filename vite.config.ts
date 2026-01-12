5cd4da5c662a2c440e3916454e37a24949537e53
neo <254613259@qq.com>
2026-01-12 14:05:51 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
         overlay: false,
       },
     },
-    base: "./",
+    base: "/",
     plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
     resolve: {
       alias: {
